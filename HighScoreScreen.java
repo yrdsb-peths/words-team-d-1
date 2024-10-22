fbc2897bb694934ecafac8c0712b779972dbf5ca
cyc9887 <cyc9887@gmail.com>
2024-10-22 12:41:11 -0400

    created high score screen

---
+++ HighScoreScreen.java
@@ -0,0 +1,24 @@
+import greenfoot.*;
+public class HighScoreScreen extends World
+{
+    public HighScoreScreen()
+    {    
+        super(600, 400, 1);
+        prepare();
+    }
+}
+
+public void prepare()
+{
+
+}
+
+public void act()
+{
+
+}
+
+public void updateScore()
+{
+
+}
