f16167c143db11b5158ea47bfc2ae60d3023b504
tiiffanyho <Tiffanyho0903@gmail.com>
2024-10-23 12:45:12 -0400

    instructions screen

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,21 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class InstructionScreen here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class InstructionScreen extends World
 {
-
-    /**
-     * Constructor for objects of class InstructionScreen.
-     * 
-     */
-    public InstructionScreen()
+    private World home;
+    String firstInstruction = "Instruction 1";
+    String secondInstruction = "Instruction 2";
+    String thirdInstruction = "Instruction 3";
+    private String[] allText = { firstInstruction, secondInstruction, thirdInstruction };
+    private int currentIndex = 0;
+    private Label instructionLabel = new Label(allText[currentIndex], 50);
+    
+    public InstructionScreen(World home)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        this.home = home;
+        addObject(instructionLabel, 300, 150);
+        
+        
     }
 }
