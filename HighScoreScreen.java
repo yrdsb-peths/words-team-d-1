8811cd2ac2d5c0a8fb3d4613ce7413cf975fcfdc
cyc9887 <cyc9887@gmail.com>
2024-10-25 12:41:19 -0400

    fix

--- HighScoreScreen.java
+++ HighScoreScreen.java
@@ -53,11 +53,7 @@ public class HighScoreScreen extends World
 
     public void updateScore()
     {
-        if (getObjects(Label.class).size() > 1) {
-            // get 0 returns title label
-            // get 2 returns back to menu label
-            removeObject(getObjects(Label.class).get(1));
-        }
+        removeObject(scoreLabel);
 
         String currentPlayer = players.get(index);
         Integer currentScore = highScores.get(currentPlayer);
