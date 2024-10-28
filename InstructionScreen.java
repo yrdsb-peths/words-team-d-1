88043b67817b6bae4efdfd892c79af286428d8b3
somethingsomethingtwice <167238930+somethingsomethingtwice@users.noreply.github.com>
2024-10-28 13:14:56 -0400

    Make instructions use wrapping

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -4,11 +4,11 @@ public class InstructionScreen extends World
 {
     private World home;
     String firstInstruction = "Test how fast you can type!!";
-    String secondInstruction = "Follow the paragraph given and type your fastest \n without making any mistakes!";
-    String thirdInstruction = "At the end you will see your \n typing speed and your error!";
+    String secondInstruction = "Follow the paragraph given and type your fastest without making any mistakes!";
+    String thirdInstruction = "At the end you will see your typing speed and your error!";
     private String[] allText = { firstInstruction, secondInstruction, thirdInstruction };
     private int currentIndex = 0;
-    private Label instructionLabel = new Label(allText[currentIndex], 40);
+    private Label instructionLabel = new Label(allText[currentIndex], 40, true);
     private Label prev;
     private Label next;
     private Label backMenuLabel;
