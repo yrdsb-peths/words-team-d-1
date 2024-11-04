61f0bd61a0994475fcd8c2b009e5e93200bda850
tiiffanyho <Tiffanyho0903@gmail.com>
2024-11-04 13:23:10 -0500

    create result screen

--- ResultScreen.java
+++ ResultScreen.java
@@ -8,16 +8,39 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ResultScreen extends World
 {
-
-    /**
-     * Constructor for objects of class ResultScreen.
-     * 
-     */
-    public ResultScreen()
+    private Label backMenuLabel;
+    private Label playAgain;
+    
+    ResultScreen(String typedText, String paragraph, int timeInSec)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        setBackground(new GreenfootImage("background.png"));
         
+        int wordsTyped = typedText.split(" ").length;     // Count words typed
+        int wordsWrong = calculateWordsWrong(typedText, paragraph); // Calculate wrong words
         
+        WPMCalculation wpmCalculation = new WPMCalculation();
+        addObject(wpmCalculation, 0, 0); // Add off-screen initially
+        wpmCalculation.updateStats(wordsTyped, timeInSec, wordsWrong);
+    }
+    
+    private int calculateWordsWrong(String typedText, String paragraph)
+    {
+        String[] typedWords = typedText.split(" ");
+        String[] paragraphWords = paragraph.split(" ");
+        int wrongWords = 0;
+        
+        for (int i = 0; i < typedWords.length; i++) {
+            if (i >= paragraphWords.length || !typedWords[i].equals(paragraphWords[i])) {
+                wrongWords++;
+            }
+        }
+        
+        if (typedWords.length > paragraphWords.length) {
+            wrongWords += typedWords.length - paragraphWords.length;
+        }
+        
+        return wrongWords;
     }
 }
