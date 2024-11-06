cd5998b279c5157dc4e70a23df4d84df2f587142
tiiffanyho <Tiffanyho0903@gmail.com>
2024-11-05 21:12:44 -0500

    remove next button when no next

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -12,7 +12,7 @@ public class InstructionScreen extends World
     private Label prev;
     private Label next;
     private Label backMenuLabel;
-    
+    Button nextButton = new Button(this::nextScreen);
     public InstructionScreen(World home)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -21,7 +21,7 @@ public class InstructionScreen extends World
         addObject(instructionLabel, 300, 150);
         setBackground(new GreenfootImage("background.png"));
         
-        addObject(new Button(this::nextScreen), 500, 300);
+        addObject(nextButton, 500, 300);
         next = new Label("next", 20);
         addObject(next, 500, 265);
         
@@ -44,6 +44,11 @@ public class InstructionScreen extends World
         if (currentIndex < allText.length - 1) {
             currentIndex++;
             instructionLabel.setValue(allText[currentIndex]);
+        }
+        
+        if (currentIndex == allText.length - 1) {
+            removeObject(next); 
+            removeObject(nextButton); 
         }
     }
     
